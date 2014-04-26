d793facb819241e5f00670fc4e2464798a76c5cd
Martín Rodríguez <martin.roosp@gmail.com>
2014-04-26 16:19:54 -0300

    TDD GeneradorDeSugerencias.

--- src/untref/aydoo/tierramedia/Coordenada.java
+++ src/untref/aydoo/tierramedia/Coordenada.java
@@ -2,4 +2,23 @@ package untref.aydoo.tierramedia;
 
 public class Coordenada {
 
+	private double latitud;
+	private double longitud;
+
+	public double getLatitud() {
+		return latitud;
+	}
+
+	public void setLatitud(double latitud) {
+		this.latitud = latitud;
+	}
+
+	public double getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(double longitud) {
+		this.longitud = longitud;
+	}
+
 }
---
+++ test/untref/aydoo/tierramedia/GeneradorDeSugerenciasTest.java
@@ -0,0 +1,12 @@
+package untref.aydoo.tierramedia;
+
+import org.junit.Test;
+
+public class GeneradorDeSugerenciasTest {
+
+	@Test
+	public void generarSugerencia() {
+
+	}
+
+}
