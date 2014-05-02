09daca44d62581ce168e3a1b501dc04d06efa895
ebeneventano <emmanuel.beneventano@gmail.com>
2014-05-02 19:13:00 -0300

    Clase de test de la promocion extranjero.

---
+++ test/untref/aydoo/tierramedia/test/promocion/PromocionExtranjeroTest.java
@@ -0,0 +1,27 @@
+package untref.aydoo.tierramedia.test.promocion;
+import org.junit.Assert;
+import org.junit.Test;
+
+import untref.aydoo.tierramedia.Atraccion;
+import untref.aydoo.tierramedia.Coordenada;
+import untref.aydoo.tierramedia.Usuario;
+
+public class PromocionExtranjeroTest {
+
+	@Test
+	public void testLocacionDelClienteMayorA200Km(){
+//		Usuario usuario = new Usuario();
+//		int latitudUsuario = 100;
+//		int longitudUsuario = 500;
+//		Coordenada coordenadaUsuario = new Coordenada(latitudUsuario, longitudUsuario);
+//		usuario.setCoordenadas(coordenadaUsuario);
+//		
+//		Atraccion atraccion = new Atraccion();
+//		int latitudAtraccion = 200;
+//		int longitudAtraccion = 1000;
+//		Coordenada coordenadaAtraccion = new Coordenada(latitudAtraccion, longitudAtraccion);
+//		atraccion.setCoordenadas(coordenadaAtraccion);
+//		
+//		Assert.assertEquals(40, atraccion.calcularDistanciaAUsuarioEnMts(usuario.getCoordenadas().getLatitud(), usuario.getCoordenadas().getLongitud()));
+	}
+}
