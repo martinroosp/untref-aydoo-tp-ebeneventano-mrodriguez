5a56352a778b1a96bdadb38d57ba68c93ef2ff88
ebeneventano <emmanuel.beneventano@gmail.com>
2014-05-03 11:02:41 -0300

    Agrego metodo que comprueba que la atraccion mas cercana al usuario que se encuentra en el obelisco, es el aeroparque jorge newbery y no temaiken ni el glaciar perito moreno.

--- test/untref/aydoo/tierramedia/test/promocion/PromocionExtranjeroTest.java
+++ test/untref/aydoo/tierramedia/test/promocion/PromocionExtranjeroTest.java
@@ -1,12 +1,40 @@
 package untref.aydoo.tierramedia.test.promocion;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import untref.aydoo.tierramedia.Atraccion;
 import untref.aydoo.tierramedia.Coordenada;
 import untref.aydoo.tierramedia.Usuario;
+import untref.aydoo.tierramedia.promocion.PromocionExtranjero;
 
 public class PromocionExtranjeroTest {
+	
+	private Atraccion atraccionA;
+	private Atraccion atraccionB;
+	private Atraccion atraccionC;
+
+	@Before
+	public void before() {
+
+		atraccionA = new Atraccion();
+		atraccionB = new Atraccion();
+		atraccionC = new Atraccion();
+
+		atraccionA.setCosto(100);
+		//Coordenadas temaiken
+		atraccionA.setCoordenadas(new Coordenada(-34.365953, -58.806653));
+		atraccionA.setNombre("Temaiken");
+		
+		atraccionB.setCosto(200);
+		//Coordenadas glaciar perito moreno
+		atraccionB.setCoordenadas(new Coordenada(-32.9271118, -68.7838236));
+		atraccionB.setNombre("Glaciar Perito Moreno");
+		
+		atraccionC.setCosto(300);
+		atraccionC.setCoordenadas(new Coordenada(-34.55803, -58.417009));
+		atraccionC.setNombre("Aeroparque Jorge Newbery");
+	}
 
 	@Test
 	public void testLocacionDelClienteMayorA200Km(){
@@ -28,5 +56,26 @@ public class PromocionExtranjeroTest {
 		
 		Assert.assertTrue(atraccion.calcularDistanciaAUsuario(usuario.getCoordenadas().getLatitud(), 
 				usuario.getCoordenadas().getLongitud(), 'K') > 200);
+	}
+	
+	@Test
+	public void aeroparqueJorgeNewberyEsLaAtraccionMasCercanaAlUsuario(){
+		Usuario usuario = new Usuario();
+		//Latitud y longitud de la ubicacion del obelisco
+		double latitudUsuario = -34.603711;
+		double longitudUsuario = -58.381585;
+		
+		Coordenada coordenadaUsuario = new Coordenada(latitudUsuario, longitudUsuario);
+		usuario.setCoordenadas(coordenadaUsuario);
+		
+		int distanciaUsuarioAJorgeNewbery = atraccionC.calcularDistanciaAUsuario(latitudUsuario, longitudUsuario, 'K');
+		
+		PromocionExtranjero promocionExt = new PromocionExtranjero();
+		promocionExt.setAtraccionAPromocion(atraccionA);
+		promocionExt.setAtraccionAPromocion(atraccionB);
+		promocionExt.setAtraccionAPromocion(atraccionC);
+		
+		Assert.assertEquals(distanciaUsuarioAJorgeNewbery, 
+				promocionExt.obtenerDistanciaMasCercanaAUsuario(usuario));
 	}
 }
