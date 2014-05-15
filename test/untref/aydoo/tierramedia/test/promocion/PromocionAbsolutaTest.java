5633dfef7b521859fc03ec3cca48a45ecb6ba986
Zetakisve <martin.roosp@gmail.com>
2014-05-15 11:00:41 -0300

    TDD PromocionAbsoluta.

--- test/untref/aydoo/tierramedia/test/promocion/PromocionAbsolutaTest.java
+++ test/untref/aydoo/tierramedia/test/promocion/PromocionAbsolutaTest.java
@@ -79,4 +79,53 @@ public class PromocionAbsolutaTest {
 
 	}
 
+	@Test
+	public void getValorRetornaCostoConDescuentoSiPaqueteTieneAtraccionesNecesarias() {
+
+		PromocionAbsoluta promocion = new PromocionAbsoluta();
+		promocion.setPeriodoVigencia(new Date());
+		promocion.setValor(200);
+
+		Atraccion atraccion1 = new Atraccion();
+		Atraccion atraccion2 = new Atraccion();
+
+		atraccion1.setCosto(150);
+		atraccion2.setCosto(80);
+
+		promocion.getAtracciones().add(atraccion1);
+		promocion.getAtracciones().add(atraccion2);
+
+		List<Atraccion> paquete = new LinkedList<Atraccion>();
+		paquete.add(atraccion1);
+		paquete.add(atraccion2);
+
+		Assert.assertEquals(200, promocion.getCosto(paquete), DELTA);
+	}
+
+	@Test
+	public void getValorRetornaCostoDeLasAtraccionesSiPaqueteNoTieneAtraccionesNecesarias() {
+
+		PromocionAbsoluta promocion = new PromocionAbsoluta();
+		promocion.setPeriodoVigencia(new Date());
+		promocion.setValor(200);
+
+		Atraccion atraccion1 = new Atraccion();
+		Atraccion atraccion2 = new Atraccion();
+		Atraccion atraccion3 = new Atraccion();
+
+		atraccion1.setCosto(150);
+		atraccion2.setCosto(80);
+		atraccion3.setCosto(120);
+
+		promocion.getAtracciones().add(atraccion1);
+		promocion.getAtracciones().add(atraccion2);
+		promocion.getAtracciones().add(atraccion3);
+
+		List<Atraccion> paquete = new LinkedList<Atraccion>();
+		paquete.add(atraccion1);
+		paquete.add(atraccion2);
+
+		Assert.assertEquals(230, promocion.getCosto(paquete), DELTA);
+	}
+
 }
